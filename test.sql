025197e9f9907edcca113df95514eaf52e3f7ee2
theotor83 <152283855+theotor83@users.noreply.github.com>
2023-11-28 14:12:34 +0100

    Update test.sql

--- test.sql
+++ test.sql
@@ -45,3 +45,34 @@ PRIMARY KEY (Nom, NomSkill));
 CREATE TABLE Monstre(
 Nom VARCHAR(10) REFERENCES ENTITE PRIMARY KEY,
 ArgentDrop INTEGER);
+
+INSERT INTO SKILL
+VALUES('Attaque Basique','Offensif',1),
+('Boule Magique', 'Offensif', 1.2),
+('Morsure', 'Offensif', 1.1),
+('Soin', 'Soin', 20),
+('Grand Soin', 'Soin', 50),
+('Tres Grand Soin', 'Soin', 100);
+
+INSERT INTO OBJET VALUES
+('Talisman', 'Augmente_Vie', 0.1, 200), 
+('Amulette', 'Augmente_Def', 0.2, 250), 
+('Baton', 'Augmente_Atk', 0.2, 350);
+
+INSERT INTO ENTITE VALUES
+    ("Bertrand", 200, 35, 20, 'A'),
+    ("Roseline", 150, 45, 15, 'A'),
+    ("Igor", 350, 20, 45, 'A'),
+    ("Giselle", 400, 50, 50, 'A'),
+    
+    ("Loup", 100, 30, 5, 'M'),
+    ("Ogre", 200, 40, 15, 'M'),
+    ("Gobelin", 70, 55, 3, 'M'),
+    ("Blob", 50, 10, 0, 'M'),
+    ("Ours", 150, 50, 20, 'M'),
+    ("Sirene", 100, 50, 10, 'M'),
+    ("Dragon", 400, 250, 90, 'M'),
+    ("Cyclope", 250, 100, 70, 'M');
+
+INSERT INTO JOUEUR VALUES
+    ("Player", 100);

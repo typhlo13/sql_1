cbac0d577dfd6281b0dec72dc6956529ce66943b
theotor83 <152283855+theotor83@users.noreply.github.com>
2023-12-10 15:26:00 +0100

    AchatObjet + AchatPerso

--- test.sql
+++ test.sql
@@ -1,50 +1,118 @@
+DROP TABLE IF EXISTS JOUEUR;
 CREATE TABLE JOUEUR(
 NomJoueur VARCHAR(10) PRIMARY KEY,
 ArgentJoueur INTEGER DEFAULT 100) ;
 
+DROP TABLE IF EXISTS OBJET;
 CREATE TABLE OBJET(
 NomObjet VARCHAR(10) PRIMARY KEY,
 TypeObjet VARCHAR(10),
 Effet real,
 CoutObjet INTEGER) ;
 
-CREATE DOMAIN typeEntite
-AS CHAR(1)
-CHECK (VALUE IN ('M', 'A'));
-
-CREATE TABLE ENTITE(Nom VARCHAR(10) PRIMARY KEY,
+DROP TABLE IF EXISTS ENTITE;
+CREATE TABLE ENTITE(
+Nom VARCHAR(10) PRIMARY KEY,
 PVmax INTEGER,
 Attaque INTEGER,
 Defense INTEGER,
-LettreType typeEntite);
+LettreType CHAR(1) CHECK (LettreType IN ('M', 'A')));
 
-CREATE TABLE SKILL (NomSkill VARCHAR(10) PRIMARY KEY,
+DROP TABLE IF EXISTS SKILL;
+CREATE TABLE SKILL (
+NomSkill VARCHAR(10) PRIMARY KEY,
 TypeSkill VARCHAR(10),
 EffetSkill real);
 
-CREATE TABLE ObjetAchete(NomJoueur VARCHAR(10) REFERENCES JOUEUR,
+DROP TABLE IF EXISTS ObjetAchete;
+CREATE TABLE ObjetAchete(
+NomJoueur VARCHAR(10) REFERENCES JOUEUR,
 NomObjet VARCHAR(10) REFERENCES OBJET,
 qte INTEGER NOT NULL,
-PRIMARY KEY(NomJoueur, NomObjet));
+PRIMARY KEY(NomJoueur, NomObjet),
+FOREIGN KEY (NomJoueur) REFERENCES JOUEUR(NomJoueur),
+FOREIGN KEY (NomObjet) REFERENCES OBJET(NomObjet));
 
+DROP TABLE IF EXISTS MagazinPerso;
 CREATE TABLE MagazinPerso(
 Nom VARCHAR(10) REFERENCES ENTITE,
 NomJoueur VARCHAR(10) REFERENCES Joueur,
 CoutAllie INTEGER,
-PRIMARY KEY(Nom, NomJoueur));
+PRIMARY KEY(Nom, NomJoueur),
+FOREIGN KEY (Nom) REFERENCES ENTITE(Nom),
+FOREIGN KEY (NomJoueur) REFERENCES JOUEUR(NomJoueur));
 
+DROP TABLE IF EXISTS PersoPossede;
 CREATE TABLE PersoPossede(
 NomJoueur VARCHAR(10) REFERENCES Joueur,
 Nom VARCHAR(10) REFERENCES ENTITE,
-PRIMARY KEY(NomJoueur,Nom));
+PRIMARY KEY(NomJoueur,Nom),
+FOREIGN KEY (NomJoueur) REFERENCES JOUEUR(NomJoueur),
+FOREIGN KEY (Nom) REFERENCES ENTITE(Nom));
 
-CREATE TABLE SkillEntite(Nom VARCHAR(10) REFERENCES ENTITE,
+DROP TABLE IF EXISTS SkillEntite;
+CREATE TABLE SkillEntite(
+Nom VARCHAR(10) REFERENCES ENTITE,
 NomSkill VARCHAR(10) REFERENCES SKILL,
-PRIMARY KEY (Nom, NomSkill));
+PRIMARY KEY (Nom, NomSkill),
+FOREIGN KEY (Nom) REFERENCES ENTITE(Nom),
+FOREIGN KEY (NomSkill) REFERENCES SKILL(NomSkill));
 
+DROP TABLE IF EXISTS Monstre;
 CREATE TABLE Monstre(
 Nom VARCHAR(10) REFERENCES ENTITE PRIMARY KEY,
-ArgentDrop INTEGER);
+ArgentDrop INTEGER,
+IdMontre INTEGER,
+FOREIGN KEY (Nom) REFERENCES ENTITE(Nom));
+
+/* ========================= DEBUT TRIGGERS ========================= */
+
+DROP TRIGGER if EXISTS AchatObjet;
+CREATE TRIGGER AchatObjet
+BEFORE UPDATE ON ObjetAchete
+WHEN (SELECT ArgentJoueur FROM JOUEUR WHERE NomJoueur = 'Player') >= ((SELECT CoutObjet FROM OBJET WHERE NomObjet = NEW.NomObjet) * (NEW.qte - OLD.qte))
+BEGIN
+   UPDATE ObjetAchete
+   SET qte = qte + NEW.qte
+   WHERE NomObjet = NEW.NomObjet;
+   UPDATE JOUEUR
+   SET ArgentJoueur = (SELECT ArgentJoueur FROM JOUEUR WHERE NomJoueur = 'Player') - ((SELECT CoutObjet FROM OBJET WHERE NomObjet = NEW.NomObjet) * (NEW.qte - OLD.qte));
+END;
+
+DROP TRIGGER IF EXISTS AchatObjetRefuse;
+CREATE TRIGGER AchatObjetRefuse
+BEFORE UPDATE ON ObjetAchete
+WHEN (SELECT ArgentJoueur FROM JOUEUR WHERE NomJoueur = 'Player') < ((SELECT CoutObjet FROM OBJET WHERE NomObjet = NEW.NomObjet) * (NEW.qte - OLD.qte))
+BEGIN
+	SELECT CASE
+		WHEN NEW.qte <> OLD.qte THEN
+			RAISE (ABORT,"Vous n'avez pas assez d'argent.")
+	END;
+END;
+
+DROP TRIGGER IF EXISTS AchatPersoRefuse;
+CREATE TRIGGER AchatPersoRefuse
+BEFORE INSERT ON PersoPossede
+WHEN (SELECT ArgentJoueur FROM JOUEUR WHERE NomJoueur = NEW.NomJoueur) < (SELECT CoutAllie FROM MagazinPerso WHERE NomJoueur = NEW.NomJoueur AND Nom = NEW.Nom)
+BEGIN
+	SELECT RAISE(ABORT,"Vous n'avez pas assez d'argent.");
+END;
+
+DROP TRIGGER IF EXISTS AchatPerso;
+CREATE TRIGGER AchatPerso
+BEFORE INSERT ON PersoPossede
+WHEN (SELECT ArgentJoueur FROM JOUEUR WHERE NomJoueur = NEW.NomJoueur) >= 
+    (SELECT CoutAllie FROM MagazinPerso WHERE NomJoueur = NEW.NomJoueur AND Nom = NEW.Nom)
+BEGIN
+  UPDATE JOUEUR
+  SET ArgentJoueur = (SELECT ArgentJoueur FROM JOUEUR WHERE NomJoueur = NEW.NomJoueur) - 
+                    (SELECT CoutAllie FROM MagazinPerso WHERE NomJoueur = NEW.NomJoueur AND Nom = NEW.Nom);
+END;
+
+
+
+
+/* ========================= FIN TRIGGERS  ========================= */
 
 INSERT INTO SKILL
 VALUES('Attaque Basique','Offensif',1),
@@ -77,9 +145,6 @@ INSERT INTO ENTITE VALUES
 INSERT INTO JOUEUR VALUES
     ("Player", 100);
 
-INSERT INTO PersoPossede
-VALUES('Player','Bertrand');
-
 INSERT INTO Monstre
 VALUES('Blob',5,1),
 ('Gobelin',15,2),
@@ -89,3 +154,47 @@ VALUES('Blob',5,1),
 ('Ogre',60,6),
 ('Cyclope',120,7),
 ('Dragon',250,8);
+
+INSERT INTO SkillEntite
+VALUES("Bertrand","Attaque Basique"),
+("Roseline","Attaque Basique"),
+("Igor","Attaque Basique"),
+("Giselle","Attaque Basique"),
+("Loup","Attaque Basique"),
+("Ogre","Attaque Basique"),
+("Gobelin","Attaque Basique"),
+("Blob","Attaque Basique"),
+("Ours","Attaque Basique"),
+("Sirene","Attaque Basique"),
+("Dragon","Attaque Basique"),
+("Cyclope","Attaque Basique"),
+("Bertrand","Soin"),
+("Roseline","Soin"),
+("Igor","Soin"),
+("Giselle","Soin"),
+("Bertrand","Grand Soin"),
+("Roseline","Grand Soin"),
+("Igor","Grand Soin"),
+("Giselle","Grand Soin"),
+("Igor","Tres Grand Soin"),
+("Giselle","Tres Grand Soin"),
+("Roseline","Boule Magique"),
+("Giselle","Boule Magique"),
+("Loup","Morsure"),
+("Gobelin","Morsure"),
+("Ours","Morsure"),
+("Dragon","Morsure");
+
+INSERT INTO ObjetAchete
+VALUES("Player","Talisman",0),
+("Player","Amulette",0),
+("Player","Baton",0);
+
+INSERT INTO MagazinPerso VALUES
+	("Bertrand","Player",0),
+	("Roseline","Player",1250),
+	("Igor","Player",1500),
+	("Giselle","Player",3000);
+	
+INSERT INTO PersoPossede VALUES
+	("Player","Bertrand");
